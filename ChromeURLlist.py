3a360e8c2200f7c438369827b9f1594383fd9643
Anton Samsonov <anthonsamsonov@gmail.com>
2018-03-15 21:56:04 -0700

    Update ChromeURLlist.py

--- ChromeURLlist.py
+++ ChromeURLlist.py
@@ -1,7 +1,11 @@
 import webbrowser
-  
+import time
+
 chrome = 'open -a /Applications/Google\ Chrome.app %s'
 
-with open("urls.txt") as f:
-    for url in f:
-        webbrowser.get(chrome).open_new_tab(url.strip())
+while True:
+        print "Working hard to reproduce it..."
+        with open("urls.txt") as f:
+                for url in f:
+                        webbrowser.get(chrome).open_new_tab(url.strip())
+                        time.sleep(1)
